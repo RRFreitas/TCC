6fbacc50a9c00f8571c4bfda2dcf3672eb27fead
Rennan Rocha <rennanfreitas11@gmail.com>
2018-09-19 20:10:17 -0300

    Melhorando algoritmo com biblioteca face_recognition e adicionando algoritmo com openCV para leitura de webcam

---
+++ FaceEmbeddings.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+from PIL import Image, ImageDraw, ImageFont
+import face_recognition
+import os
+
+def detect_face(img):
+	faces_encode = face_recognition.face_encodings(img)
+	faces_locations = face_recognition.face_locations(img)
+
+	#faces = [img[y:y+h, x:x+w] for (x, y, w h) in face_locations]
+
+	return faces_encode, faces_locations
+
+def prepare_training_data(data_folder_path):
+	# pega as imagens no diretório
+	subject_images_names = os.listdir(data_folder_path)
+
+	# lista de encodings das faces
+	encodings = []
+
+	# lista de posições das faces
+	locations = []
+
+	# lista de nomes
+	names = []
+
+	for image_name in subject_images_names:
+		if(image_name.startswith('.')):
+			continue
+
+		# caminho da imagem
+		image_path = data_folder_path + "/" + image_name
+
+		image = face_recognition.load_image_file(image_path)
+
+		faces_encodes, faces_locations = detect_face(image)
+
+		print("Training image: " + image_name)
+
+		if(len(faces_encodes) > 0):
+			# É esperado que nas imagens só tenham uma face, logo face de index 0
+			face_encode = faces_encodes[0]
+			encodings.append(face_encode)
+			names.append(image_name.split('.')[0])
+
+	return encodings, names
+
+def recognize(encodings, nomes, image):
+    # font pillow
+    fnt = ImageFont.truetype('FreeSansBold.ttf', 18)
+
+    # Pega o encoding de todas as faces da foto
+    faces_encodings = face_recognition.face_encodings(image)
+    # Pega as localizações das faces na foto (top, right, bottom, left)
+    face_locations = face_recognition.face_locations(image)
+
+    pil_image = Image.fromarray(image)
+    draw = ImageDraw.Draw(pil_image)
+
+    # Percorre cada face da foto
+    for index, face_encoding in enumerate(faces_encodings):
+        results = face_recognition.compare_faces(encodings, face_encoding)
+
+        # lista de nomes das pessoas reconhecidas
+        recognizeds = [nomes[i] for i, known in enumerate(results) if known]
+        print(recognizeds)
+
+        for name in recognizeds:
+            top, right, bottom, left = face_locations[index]  # locations da face
+
+            # Desenha retângulo nos locations da face
+            draw.rectangle(((left, top), (right, bottom)), outline=(0, 255, 0))
+            draw.text((left, top - 20), name, (0, 255, 0), fnt)
+
+    return pil_image
+
+def main():
+    print("Preparing data...")
+
+    encodings, names = prepare_training_data("known_people")
+
+    image_folder = "image_folder"
+    pics = os.listdir(image_folder)
+
+    for pic in pics:
+        # Carrega foto
+        image_array = face_recognition.load_image_file(image_folder + "/" + pic)
+        pil_image = recognize(encodings, names, image_array)
+        pil_image.show()
+
+if __name__ == '__main__':
+	main()
