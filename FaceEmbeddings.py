42a6c0d95e5617fe496748234f6c7fbda7aaf51d
Rennan Rocha <rennanfreitas11@gmail.com>
2018-11-12 22:00:58 -0300

    Adicionando comentários e mais imagens.

--- FaceEmbeddings.py
+++ FaceEmbeddings.py
@@ -5,46 +5,57 @@ import face_recognition
 import os
 
 def detect_face(img):
-	faces_encode = face_recognition.face_encodings(img)
-	faces_locations = face_recognition.face_locations(img)
 
-	#faces = [img[y:y+h, x:x+w] for (x, y, w h) in face_locations]
+    # medidas de todas as faces encontradas
+    faces_encode = face_recognition.face_encodings(img)
+    # localização de todas as faces encontradas (x,y,w,h)
+    faces_locations = face_recognition.face_locations(img)
 
-	return faces_encode, faces_locations
+    #faces = [img[y:y+h, x:x+w] for (x, y, w h) in face_locations]
+
+    return faces_encode, faces_locations
 
 def prepare_training_data(data_folder_path):
-	# pega as imagens no diretório
-	subject_images_names = os.listdir(data_folder_path)
+    # lista com nome das imagens no diretório
+    subject_images_names = os.listdir(data_folder_path)
 
-	# lista de encodings das faces
-	encodings = []
+    # lista de encodings das faces
+    encodings = []
 
-	# lista de posições das faces
-	locations = []
+    # lista de posições das faces
+    locations = []
 
-	# lista de nomes
-	names = []
+    # lista de nomes
+    names = []
 
-	for image_name in subject_images_names:
-		if(image_name.startswith('.')):
-			continue
+    # percorre lista de imagens
+    for image_name in subject_images_names:
+        if(image_name.startswith('.')): # se for arquivo do sistema, pular
+            continue
 
 		# caminho da imagem
-		image_path = data_folder_path + "/" + image_name
+        image_path = data_folder_path + "/" + image_name
 
-		image = face_recognition.load_image_file(image_path)
+        # leitura da imagem passado o caminho por parâmetro
+        image = face_recognition.load_image_file(image_path)
 
-		faces_encodes, faces_locations = detect_face(image)
+        # medidas e localizações de todas as faces encontradas na imagem
+        faces_encodes, faces_locations = detect_face(image)
 
-		print("Training image: " + image_name)
+        print("Training image: " + image_name)
 
-		if(len(faces_encodes) > 0):
-			# É esperado que nas imagens só tenham uma face, logo face de index 0
-			face_encode = faces_encodes[0]
-			encodings.append(face_encode)
-			names.append(image_name.split('.')[0])
+        if(len(faces_encodes) > 0):
+            if(len(faces_encodes) > 1):
+                print("[WARN] Imagem %s contém mais de uma face!" % image_name)
 
-	return encodings, names
+            # É esperado que nas imagens só tenham uma face, logo face de index 0
+            face_encode = faces_encodes[0]
+            # Adicionar medidas da face na lista
+            encodings.append(face_encode)
+            # Adicionar nome do indivíduo na lista de nomes, baseado no nome do arquivo
+            names.append(image_name.split('.')[0])
+
+    return encodings, names
 
 def recognize(encodings, nomes, image):
     # font pillow
@@ -60,6 +71,7 @@ def recognize(encodings, nomes, image):
 
     # Percorre cada face da foto
     for index, face_encoding in enumerate(faces_encodings):
+        # Compara a face 1:1
         results = face_recognition.compare_faces(encodings, face_encoding)
 
         # lista de nomes das pessoas reconhecidas
@@ -78,15 +90,25 @@ def recognize(encodings, nomes, image):
 def main():
     print("Preparing data...")
 
-    encodings, names = prepare_training_data("known_people")
+    # Diretório com imagens pessoas conhecidas para treinar o algoritmo
+    training_data_dir = "known_people"
+    # Diretório com imagens de testes para reconhecimento de faces conhecidas
+    image_test_dir = "image_folder"
 
-    image_folder = "image_folder"
-    pics = os.listdir(image_folder)
+    # Medidas da face e nome das pessoas registradas (no diretório passado por parâmetro)
+    encodings, names = prepare_training_data(training_data_dir)
 
+    # Lista de nomes de imagens no diretório de teste
+    pics = os.listdir(image_test_dir)
+
+    # Percorrer lista de imagens de teste
     for pic in pics:
         # Carrega foto
-        image_array = face_recognition.load_image_file(image_folder + "/" + pic)
+        image_array = face_recognition.load_image_file(image_test_dir + "/" + pic)
+
+        # Reconhece e desenha as faces na foto
         pil_image = recognize(encodings, names, image_array)
+        # Mostra foto
         pil_image.show()
 
 if __name__ == '__main__':
