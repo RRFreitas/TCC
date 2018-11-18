58c47d70c1dae2e9152fac7c20d7d62c84c5422a
Rennan Rocha <rennanfreitas11@gmail.com>
2018-11-18 09:00:38 -0300

    Adicionando primary key em Encoding

--- server/models/Encoding.py
+++ server/models/Encoding.py
@@ -10,15 +10,17 @@ encoding_fields = {
 class Encoding(db.Model):
     __tablename__ = 'encodings'
 
-    medidas = db.Column('medidas', db.ARRAY(db.Integer, 128))
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    medidas = db.Column('medidas', db.ARRAY(db.Float, 128))
 
     pessoa_id = db.Column('pessoa_id', db.ForeignKey("pessoas.id"))
 
     pessoa = db.relationship("Pessoa")
 
-    def __init__(self, pessoa_id, medidas):
+    def __init__(self, pessoa_id, medidas, id=None):
         self.pessoa_id = pessoa_id
         self.medidas = medidas
+        self.id = id
 
     def __repr__(self):
         return "<Encoding [pessoa_id = %s, medidas = %s]>" % (self.pessoa_id, self.medidas)
